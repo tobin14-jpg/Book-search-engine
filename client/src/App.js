49eb36cbc2789186b05468c39dd60a79ade0e36a
Ice Tea <shauntobin88@gmail.com>
2021-12-12 20:33:10 +0800

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import {
-  ApolloClient,
-  ApolloProvider,
-} from '@apollo/client';
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
