9848b1cb2b98761e4678970a79899c7c8408f20e
Ice Tea <shauntobin88@gmail.com>
2021-12-12 22:20:04 +0800

    changed resolvers query around User.findOne id

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -11,7 +11,7 @@ const resolvers = {
         me: async (parent, args, context) => {
 
             if(context.user) {
-                const userData = await User.findOne({})
+                const userData = await User.findOne({_id: context.user._id})
                 .select('-__v -password')
                 .populate('books')
             
